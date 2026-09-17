21e31f65e66def6e4f1c9edc5b5ccfdfb8e11a4f
Samuca <samuel.mellomail@gmail.com>
2026-09-17 19:15:52 -0300

    fix: muda o host da uri para conexao com container

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,11 @@ import { configDotenv } from "dotenv"; configDotenv({quiet: true});
 
 const environment = process.env;
 
-const create_database_url = () => `postgresql://${environment.POSTGRES_USER}:${environment.POSTGRES_PASSWORD}@db:5432/${environment.POSTGRES_DB}`
+const create_database_url = () => {
+    const host = process.argv.includes("--dev") ? "localhost" : "db"
+    
+    return `postgresql://${environment.POSTGRES_USER}:${environment.POSTGRES_PASSWORD}@${host}:5432/${environment.POSTGRES_DB}`
+}
 
 
 export const DATABASE_URL = create_database_url()
